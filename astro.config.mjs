13bc46ce6fef8b96bb91e9359f9bd5c882013067
gilgonzalez <fragilgon@gmail.com>
2025-07-30 13:15:25 +0200

    resolve version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
